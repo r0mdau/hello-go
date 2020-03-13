28c35ff86a4d83dd1c3585e3525e5e630788a631
Romain Dauby <romain.dauby@gmail.com>
2020-03-13 19:01:34 +0100

    feat: nombre premier

---
+++ premier.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func premier(n int) bool {
+	if n % 2 == 0 {
+		return n == 2
+	}
+
+	for d := 3; (d * d) <= n; d += 2 {
+		if n%d == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	number, _ := strconv.Atoi(os.Args[1])
+	fmt.Println(number)
+
+	if premier(number) {
+		fmt.Println("premier !")
+	} else {
+		fmt.Println("pas un nombre premier")
+	}
+
+}
