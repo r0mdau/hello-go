eb66687d1aaf79b9dd5f01d5d8b02056506d78fd
Romain Dauby <romain.dauby@gmail.com>
2020-03-14 18:58:28 +0100

    wip: learn-go-with-tests/hello

---
+++ hello/hello.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func greetingPrefix(langage string) string {
+	switch langage {
+	case spanish:
+		return spanishHelloPrefix
+	case french:
+		return frenchHelloPrefix
+	}
+	return englishHelloPrefix
+}
+
+// Hello returns a greeting
+func Hello(name string, langage string) string {
+	if len(name) == 0 {
+		name = "World"
+	}
+	return greetingPrefix(langage) + name
+}
+
+func main() {
+	fmt.Println(Hello("Chris", "English"))
+}
---
+++ hello/hello_test.go
@@ -0,0 +1,55 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying hello to people in spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("greetingPrefix returns in english if not recognized", func(t *testing.T) {
+		got := greetingPrefix("Greek")
+		want := "Hello, "
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("greetingPrefix returns in spanish", func(t *testing.T) {
+		got := greetingPrefix("Spanish")
+		want := "Hola, "
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("greetingPrefix returns in french", func(t *testing.T) {
+		got := greetingPrefix("French")
+		want := "Bonjour, "
+
+		assertCorrectMessage(t, got, want)
+	})
+}
