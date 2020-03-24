90b8358cb4444241d10490d20d253902b337c73e
Romain Dauby <romain.dauby@gmail.com>
2020-03-24 12:53:14 +0100

    wip: mock

---
+++ mocking/README.md
@@ -0,0 +1,4 @@
+Uncle Bob:
+ - when to mock : https://blog.cleancoder.com/uncle-bob/2014/05/10/WhenToMock.html
+ - different type of mock : https://blog.cleancoder.com/uncle-bob/2014/05/14/TheLittleMocker.html
+
---
+++ mocking/countdown.go
@@ -0,0 +1,39 @@
+package mocking
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, strconv.Itoa(i))
+	}
+	sleeper.Sleep()
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
+}
