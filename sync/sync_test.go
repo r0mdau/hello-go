944390454feab088901a0eb7b8e7bd0e454c3785
Romain Dauby <romain.dauby@gmail.com>
2020-05-15 14:39:35 +0200

    wip(sync): kind of constructor \o/

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestCounter(t *testing.T) {
 	t.Run("increment 3 times the counter, leave it at 3", func(t *testing.T) {
-		counter := Counter{}
+		counter := NewCounter()
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
@@ -17,7 +17,7 @@ func TestCounter(t *testing.T) {
 
 	t.Run("it runs safely concurrently", func(t *testing.T) {
 		wantedCount := 1000
-		counter := Counter{}
+		counter := NewCounter()
 
 		var wg sync.WaitGroup
 		wg.Add(wantedCount)
@@ -34,7 +34,7 @@ func TestCounter(t *testing.T) {
 	})
 }
 
-func assertCounter(t *testing.T, counter Counter, want int) {
+func assertCounter(t *testing.T, counter *Counter, want int) {
 	t.Helper()
 	if counter.Value() != want {
 		t.Errorf("got %d, want %d", counter.Value(), want)
