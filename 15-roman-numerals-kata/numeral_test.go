5305a808b960ac230f5d74429cc39dc0271c73db
Romain Dauby <romain.dauby@gmail.com>
2020-07-26 10:31:55 +0200

    feat(kata): adding property based tests ;) uint16

--- 15-roman-numerals-kata/numeral_test.go
+++ 15-roman-numerals-kata/numeral_test.go
@@ -3,6 +3,7 @@ package _5_roman_numerals_kata
 import (
 	"fmt"
 	"testing"
+	"testing/quick"
 )
 
 var cases = []struct {
@@ -61,5 +62,23 @@ func TestConvertingToArabic(t *testing.T) {
 				t.Errorf("got %q, want %q", got, test.Arabic)
 			}
 		})
+	}
+}
+
+func TestPropertiesOfConversion(t *testing.T) {
+	assertion := func(arabic uint16) bool {
+		if arabic > 3999 {
+			return true
+		}
+		t.Log("testing", arabic)
+		roman := ConvertToRoman(arabic)
+		fromRoman := ConvertToArabic(roman)
+		return fromRoman == arabic
+	}
+
+	if err := quick.Check(assertion, &quick.Config{
+		MaxCount: 1000,
+	}); err != nil {
+		t.Error("failed checks", err)
 	}
 }
